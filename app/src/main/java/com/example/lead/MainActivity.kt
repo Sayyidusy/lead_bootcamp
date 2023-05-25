49ee37d2d8db3e361e22e82c368a80bb94a19682
Sayyidusy syauqi Al Ghiffari <sayyidusyalghiffari@gmail.com>
2023-05-25 09:53:51 +0700

    "pertemuan 2, toast login"

--- app/src/main/java/com/example/lead/MainActivity.kt
+++ app/src/main/java/com/example/lead/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.lead
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.lead.databinding.ActivityLoginBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSignin.setOnClickListener {
+            val username = binding.tilUsername.text.toString()
+            val password = binding.tilPassword.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                val successMessage = "$username berhasil login"
+                showToast(successMessage)
+            } else {
+                val errorMessage = "Anda gagal login, silahkan coba lagi"
+                showToast(errorMessage)
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
+
+
